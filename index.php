62a0d1002623a889fe986b0c44586a86d8b68767
Iman Tabrizian <tabrizian@outlook.com>
2016-06-22 15:49:34 +0430

    Adds index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+</body>
+<?php
+    $m = new MongoClient();
+    echo "Connection to database Successfuly.";
+    $db = $m->new;
+
+    echo "New database new selected.";
+?>
+</body>
+</html>
