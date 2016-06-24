45785b4f119c37453feb632ec137c4903b0470a4
Iman Tabrizian <tabrizian@outlook.com>
2016-06-24 19:29:11 +0430

    Improves the model of application

---
+++ includes/message.php
@@ -0,0 +1,7 @@
+<?php
+    class Message extends DatabaseObject {
+        public $id;
+        public $text;
+        public $from_user;
+    }
+?>
---
+++ includes/user.php
@@ -0,0 +1,9 @@
+<?php
+    class User extends DatabaseObject {
+        public $id;
+        public $first_name;
+        public $last_name;
+        public $email_address;
+        public $friends;
+    }
+?>
