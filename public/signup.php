149c61d88105b0ce74f36a829ef676ca6605f4d9
Iman Tabrizian <tabrizian@outlook.com>
2016-06-28 12:06:32 +0430

    Improves messages

--- public/signup.php
+++ public/signup.php
@@ -1,20 +1,24 @@
 <?php require_once('../includes/initialize.php'); ?>
 <?php
-    if(isset($_POST['submit'])) {
-        $user = new User();
-        $user->first_name = $_POST['first_name'];
-        $user->last_name = $_POST['last_name'];
-        $user->email_address = $_POST['email_address'];
-        $user->birth_day = strtotime($_POST['year'] + "/" + $_POST['month'] + "/" + $_POST['day']);
-        $user->username = $_POST['username'];
-        $user->password = $_POST['password'];
-        $user->sex = $_POST['sex'];
+if (isset($_POST['submit'])) {
+    $user = new User();
+    $user->first_name = $_POST['first_name'];
+    $user->last_name = $_POST['last_name'];
+    $user->email_address = $_POST['email_address'];
+    $user->birth_day = strtotime($_POST['year'] + "/" + $_POST['month'] + "/" + $_POST['day']);
+    $user->username = $_POST['username'];
+    $user->password = $_POST['password'];
+    $user->sex = $_POST['sex'];
 
-        $user->insert();
+    if ($user->insert()) {
+        echo "Signup success!";
+    } else {
+        echo "Signup failed!";
     }
+}
 ?>
 <?php include_layout_template('login-header.php'); ?>
-    <div class="container-fluid" >
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-4 col-md-offset-4  well well-sm">
                 <legend><a href=#><i class="glyphicon glyphicon-globe"></i></a> Sign up!</legend>
@@ -22,17 +26,16 @@
                     <div class="row">
                         <div class="col-xs-6 col-md-6">
                             <input class="form-control" name="first_name" placeholder="First Name" type="text"
-                                   required autofocus />
+                                   required autofocus/>
                         </div>
                         <div class="col-xs-6 col-md-6">
-                            <input class="form-control" name="last_name" placeholder="Last Name" type="text" required />
+                            <input class="form-control" name="last_name" placeholder="Last Name" type="text" required/>
                         </div>
                     </div>
-                    <input class="form-control" name="username" placeholder="User Name" type="text" />
-                    <input class="form-control" name="password" placeholder="Password" type="password" />
-                    <input class="form-control" name="email_address" placeholder="Email" type="email" />
-                    <label for="">
-                        Birth Date</label>
+                    <input class="form-control" name="username" placeholder="User Name" type="text"/>
+                    <input class="form-control" name="password" placeholder="Password" type="password"/>
+                    <input class="form-control" name="email_address" placeholder="Email" type="email"/>
+                    <label for="">Birth Date</label>
                     <div class="row">
                         <div class="col-xs-4 col-md-4">
                             <input class="form-control" name="day" placeholder="Day" type="number" min="1" max="31"/>
@@ -45,17 +48,18 @@
                         </div>
                     </div>
                     <label class="radio-inline">
-                        <input type="radio" name="sex" id="inlineCheckbox1" value="male" />
+                        <input type="radio" name="sex" id="inlineCheckbox1" value="male"/>
                         Male
                     </label>
                     <label class="radio-inline">
-                        <input type="radio" name="sex" id="inlineCheckbox2" value="female" />
+                        <input type="radio" name="sex" id="inlineCheckbox2" value="female"/>
                         Female
                     </label>
-                    <br />
-                    <br />
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">
-                        Sign up</button>
+                    <br/>
+                    <br/>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">
+                        Sign up
+                    </button>
                 </form>
             </div>
         </div>
