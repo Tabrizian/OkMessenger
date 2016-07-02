cc205a13cdf856ac48619773590ae796b999f755
Iman Tabrizian <tabrizian@outlook.com>
2016-07-02 12:52:55 +0430

    Minor bug fixed

--- includes/user.php
+++ includes/user.php
@@ -41,14 +41,10 @@ class User extends DatabaseObject
                         <div class=\"col-xs-12 col-sm-3\">
                             <img src=\"{$this->image}\" alt=\"{$this->full_name()}\" class=\"img-responsive img-circle\" />
 
-                            <div class=\"checkbox\">
-                                       <label><input type=\"checkbox\" value=\"\">send unknown </label>
-                              </div>
-
                             <a href='add_friend.php?id=$this->_id'>Add friend</a>
                          </div>
 
-                        </div>
+                    
                         <div class=\"col-xs-12 col-sm-9\">
                              
                             <span class=\"name\">{$this->full_name()}</span><br/>
