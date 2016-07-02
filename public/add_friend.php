c8ac91528cc832c0325870c226f91ec9da8f6022
Iman Tabrizian <tabrizian@outlook.com>
2016-07-02 04:43:24 +0430

    Friend lists finished

--- public/add_friend.php
+++ public/add_friend.php
@@ -10,6 +10,8 @@ $user = User::find_by_id($_GET['id']);
 $current_user = User::find_by_id($session->user_id);
 if(!in_array($user->_id, $current_user->friends)) {
     $current_user->friends[] = $user->_id;
+    $current_user->update();
+    log_action("Friend added");
 }
 
 redirect_to("profile.php");
