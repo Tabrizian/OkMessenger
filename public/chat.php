56507d57155731d645774464b6a02c3f26a91fc8
Iman Tabrizian <tabrizian@outlook.com>
2016-07-07 02:22:25 +0430

    Last commit

--- public/chat.php
+++ public/chat.php
@@ -75,6 +75,8 @@ if (isset($_GET['id']) && isset($_GET['room_type'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="10" url="<?php echo $_SERVER['PHP_SELF']; ?>">
+
     <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet'
           type='text/css'>
